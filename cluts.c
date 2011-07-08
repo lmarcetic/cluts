5e791de1fdbd5ef5e6207fa3e413c469cf153d1e
Luka Marčetić <paxcoder@gmail.com>
2011-07-08 23:51:34 +0200

    tabs to spaces

--- cluts.c
+++ cluts.c
@@ -22,48 +22,48 @@
  **/
 int main(int argc, char *argv[])
 {
-	int i, n;
-	char s[PATH_MAX + FILENAME_MAX], tests_path[]="tests/";
-	struct dirent **items;
-	struct stat status;
-	int returned, all, failed;
-	
-	n = scandir(tests_path, &items, 0, alphasort);
-	if (n < 1) {
-		getcwd(s, 3);
-		fprintf(stderr, "No tests found at location \"%s/tests/\"\n", s);
-		return 1;
-	}
-	
-	failed = all = 0;
-	for (i=0; i<n; ++i) {
-		*s='\0';
-		strcat(s, tests_path);
-		strcat(s, items[i]->d_name);
-		
-		stat(s, &status);
-		if (S_ISREG(status.st_mode) && !access(s, X_OK)) {
-			++all;
-			printf("Executing '%s' test collection...\n", items[i]->d_name);
-			if (!fork() && execv(s, argv)==-1)
-					return -1;
-			wait(&returned);
-			
-			printf("The '%s' test collection ", items[i]->d_name);
-		    if (!WIFEXITED(returned))
-		        printf("crashed!\n");
-			else if (WEXITSTATUS(returned) == -1)
-				printf("failed to start.\n");
-			else if (WEXITSTATUS(returned) == 0)
-				printf("passed.\n");
-			else
-				printf("failed %i test(s).\n", returned);
-			
-			if (!WIFEXITED(returned) || WEXITSTATUS(returned)!=0)
-				++failed;
-		}
-	}
-	
-	printf("\nTest collections passed: %d/%d\n", all-failed, all);
-	return 0;
+    int i, n;
+    char s[PATH_MAX + FILENAME_MAX], tests_path[]="tests/";
+    struct dirent **items;
+    struct stat status;
+    int returned, all, failed;
+    
+    n = scandir(tests_path, &items, 0, alphasort);
+    if (n < 1) {
+        getcwd(s, 3);
+        fprintf(stderr, "No tests found at location \"%s/tests/\"\n", s);
+        return 1;
+    }
+    
+    failed = all = 0;
+    for (i=0; i<n; ++i) {
+        *s='\0';
+        strcat(s, tests_path);
+        strcat(s, items[i]->d_name);
+        
+        stat(s, &status);
+        if (S_ISREG(status.st_mode) && !access(s, X_OK)) {
+            ++all;
+            printf("Executing '%s' test collection...\n", items[i]->d_name);
+            if (!fork() && execv(s, argv)==-1)
+                    return -1;
+            wait(&returned);
+            
+            printf("The '%s' test collection ", items[i]->d_name);
+            if (!WIFEXITED(returned))
+                printf("crashed!\n");
+            else if (WEXITSTATUS(returned) == -1)
+                printf("failed to start.\n");
+            else if (WEXITSTATUS(returned) == 0)
+                printf("passed.\n");
+            else
+                printf("failed %i test(s).\n", returned);
+            
+            if (!WIFEXITED(returned) || WEXITSTATUS(returned)!=0)
+                ++failed;
+        }
+    }
+    
+    printf("\nTest collections passed: %d/%d\n", all-failed, all);
+    return 0;
 }
