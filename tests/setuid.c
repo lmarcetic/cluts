934bf340f6c490efa0e56a5514628de0ba514c93
Luka Marčetić <paxcoder@gmail.com>
2011-07-16 17:22:34 +0200

    bugfixes

--- tests/setuid.c
+++ tests/setuid.c
@@ -30,7 +30,7 @@
  
 sem_t *sem; //will point to two semaphores
 int pfd[2]; //will hold file pointers for pipe-based synchronization
-static void* gotuid(void *foo);
+static void* ret_uid(void *foo);
 
 int main()
 {
@@ -38,7 +38,7 @@ int main()
     const struct rlimit   nproc     = {10, 10};
     const uid_t           uid       = MAXUID-4;
     unsigned int    i, j, mismatch;
-    int             stat, uidset, fd = open("/dev/zero", O_RDWR);
+    int             stat, uid_set, fd = open("/dev/zero", O_RDWR);
     pid_t           pid;
     pthread_t       tid[nproc.rlim_cur];
     uid_t           uids[nproc.rlim_cur];
@@ -77,13 +77,13 @@ int main()
         if (!(pid = fork())) {
             mismatch = ret = 0; //for this process, that is
             for (i=0; i<nproc.rlim_cur; ++i)
-                pthread_create(&tid[i], NULL, gotuid, &uids[i]);
+                pthread_create(&tid[i], NULL, ret_uid, &uids[i]);
             
             sem_post(&sem[1]);    //ready
             read(pfd[0], NULL, 1);//wait
             if (j%2 != 0)
                 sched_yield();
-            uidset = setuid(uid);
+            uid_set = setuid(uid);
             
             for (i=0; i<nproc.rlim_cur; ++i) {
                 pthread_join(tid[i], NULL);
@@ -92,14 +92,18 @@ int main()
             }
             if (mismatch) {
                 ret = -1;
-                if (!uidset) {
+                if (uid_set) {
                     for (i=0; i<nproc.rlim_cur && uids[i]!=0; ++i);
                     if (i < nproc.rlim_cur) {
-                        fprintf(stderr, "getuid() per threads: [");
-                        for (; i<nproc.rlim_cur; ++i)
+                        fprintf(stderr, "getuid() per threads(non-root='.', root='0'):\n[");
+                        mismatch = 0;
+                        for (i=0; i<nproc.rlim_cur; ++i) {
                             fprintf (stderr, "%c", *(".0"+(uids[i] == 0)));
-                        fprintf(stderr, "] (non-root='.', root='0')\n");
-                        ret = 1;
+                            if (uids[i] == 0)
+                                ++mismatch;
+                        }
+                        ret = mismatch;
+                        fprintf(stderr, "]\n");
                     }
                 }
             }
@@ -112,6 +116,7 @@ int main()
         sem_wait(&sem[1]);
         //allow them to continue:
         close(pfd[0]);
+        printf("Try #%i/%i\n", j+1, max_tries); //---
         
         waitpid(pid, &stat, 0);
         if(WIFEXITED(stat)) {
@@ -134,9 +139,9 @@ int main()
         else
             fprintf(
                 stderr,
-                "[Iteration %i] Process' call to setuid() succeeded, but %i of"
-                " its threads still\nreported getuid() == 0(root)!\n",
-                ret, j+1
+                "<Iteration %i> Process' call to setuid() succeeded, but %i/%i"
+                " of its threads still reported getuid() == 0(root)!\n",
+                j, ret, nproc.rlim_cur
             );
     }
     
@@ -148,7 +153,7 @@ int main()
 /** waits for pdf[0] file descriptor to close,
  ** \returns a getuid value cast to void*
  **/
-static void* gotuid(void *uid)
+static void* ret_uid(void *uid)
 {
     read(pfd[0], uid, 1);//wait
     *(uid_t*)uid = getuid();
