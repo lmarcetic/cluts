4f0a89ee0d797a7d9b9980181ed5a6311b8ffdaa
Luka Marčetić <paxcoder@gmail.com>
2011-07-16 20:49:25 +0200

    changing timing, hunt for buggy behavior continues

--- tests/setuid.c
+++ tests/setuid.c
@@ -30,14 +30,15 @@
  
 sem_t *sem; //will point to two semaphores
 int pfd[2]; //will hold file pointers for pipe-based synchronization
-static void* ret_uid(void *foo);
+static void* set_uid(void *uid);
 
 int main()
 {
-    const unsigned int    max_tries = 10000;
+    const unsigned int    max_tries = 5,
+                          max_spins = 1000;
     const struct rlimit   nproc     = {10, 10};
     const uid_t           uid       = MAXUID-4;
-    unsigned int    i, j, mismatch;
+    unsigned int    i, j, k, mismatch, *foo;
     int             stat, failed_setuid, fd = open("/dev/zero", O_RDWR);
     pid_t           pid;
     pthread_t       tid[nproc.rlim_cur];
@@ -49,6 +50,7 @@ int main()
         return 1;
     }
     sem = mmap(NULL, sizeof(sem_t)*2, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+    foo = mmap(NULL, sizeof(int),     PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
     sem_init(&sem[0], 1, 0);
     sem_init(&sem[1], 1, 0);
     setrlimit(RLIMIT_NPROC, &nproc);
@@ -63,26 +65,29 @@ int main()
     
     ret = 0;
     for (j=0; j<max_tries && ret<=0; ++j) {
+      for (k=0; k<max_spins && ret<=0; ++k) {
         pipe(pfd);
         close(pfd[1]);
         if (!fork()) {
+            setuid(uid);
             sem_post(&sem[1]);    //ready
             read(pfd[0], NULL, 1);//wait
             if (j%2 == 0)
-                sched_yield();
-            setuid(uid);
+                for(i=0; i<k; ++i)
+                    *foo ^= i;
             
             return 0;
         }
         if (!(pid = fork())) {
             mismatch = ret = 0; //for this process, that is
             for (i=0; i<nproc.rlim_cur; ++i)
-                pthread_create(&tid[i], NULL, ret_uid, &uids[i]);
+                pthread_create(&tid[i], NULL, set_uid, &uids[i]);
             
             sem_post(&sem[1]);    //ready
             read(pfd[0], NULL, 1);//wait
             if (j%2 != 0)
-                sched_yield();
+                for(i=0; i<max_spins-k; ++i)
+                    *foo ^= i;
             failed_setuid = setuid(uid);
             
             for (i=0; i<nproc.rlim_cur; ++i) {
@@ -95,7 +100,9 @@ int main()
                 if (!failed_setuid) {
                     for (i=0; i<nproc.rlim_cur && uids[i]!=0; ++i);
                     if (i < nproc.rlim_cur) {
-                        fprintf(stderr, "getuid() per threads(non-root='.', root='0'):\n[");
+                        fprintf(stderr,
+                          "getuid() per thread(non-root='.', root='0'):\n["
+                        );
                         mismatch = 0;
                         for (i=0; i<nproc.rlim_cur; ++i) {
                             fprintf (stderr, "%c", *(".0"+(uids[i] == 0)));
@@ -116,7 +123,6 @@ int main()
         sem_wait(&sem[1]);
         //allow them to continue:
         close(pfd[0]);
-        printf("Try #%i/%i\n", j+1, max_tries); //---
         
         waitpid(pid, &stat, 0);
         if(WIFEXITED(stat)) {
@@ -126,6 +132,7 @@ int main()
         else if (ret == 0)
             ret = -2;
         wait(NULL);
+      }
     }
     if (ret) {
         if (ret == -1)
@@ -141,19 +148,18 @@ int main()
                 stderr,
                 "<Iteration %u> Process' call to setuid() succeeded, but %i/%i"
                 " of its threads still reported getuid() == 0(root)!\n",
-                j, ret, nproc.rlim_cur
+                j, ret, (int)nproc.rlim_cur
             );
     }
+    printf("%s", ("\0\0"+(*foo%j))); //aka nothing
     
     sem_post(&sem[0]);
     for (i=0; i < nproc.rlim_cur-1; ++i)
         wait(NULL);
     return (ret == 0);
 }
-/** waits for pdf[0] file descriptor to close,
- ** \returns a getuid value cast to void*
- **/
-static void* ret_uid(void *uid)
+//waits for pdf[0] to close, then sets a void* to the value of getuid
+static void* set_uid(void *uid)
 {
     read(pfd[0], uid, 1);//wait
     *(uid_t*)uid = getuid();
